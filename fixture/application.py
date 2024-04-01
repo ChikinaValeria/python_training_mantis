ada0db3c28afeaff0a28379cd8beb25af3777502
Valeria Chikina <dliavajnogo@gmail.com>
2024-04-01 14:37:23 +0300

    first commit

---
+++ fixture/application.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from fixture.session import Session_helper
+from fixture.group import Group_helper
+from fixture.entry import Entry_helper
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+
+        #self.wd.implicitly_wait(60)
+        self.session = Session_helper(self)
+        self.group = Group_helper(self)
+        self.entry = Entry_helper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+
+    def destroy(self):
+        self.wd.quit()
---
+++ fixture/session.py
@@ -0,0 +1,49 @@
+import time
+from selenium.webdriver.common.by import By
+
+class Session_helper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, username, password):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element('name', "user").clear()
+        wd.find_element('name', "user").send_keys(username)
+        wd.find_element('name', "pass").click()
+        wd.find_element('name', "pass").clear()
+        wd.find_element('name', "pass").send_keys(password)
+        wd.find_element('xpath', "//input[@value='Login']").click()
+
+    def logout(self):
+        wd = self.app.wd
+        time.sleep(1)
+        wd.find_element('link text', "Logout").click()
+        time.sleep(1)
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements('link text', "Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element(By.XPATH, "//div/div[1]/form/b").text[1:-1]
