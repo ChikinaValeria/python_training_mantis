27e6d73e1d1d3a4cf5692d74cf9fc88e53569e7b
Valeria Chikina <dliavajnogo@gmail.com>
2024-04-03 17:21:30 +0300

    refactor test_delete_project.py

--- fixture/project.py
+++ fixture/project.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 from model.project import Project
-
+import time
 class Project_helper:
 
     def __init__(self, app):
@@ -26,6 +26,7 @@ class Project_helper:
 
     def create_project(self, project):
         wd = self.app.wd
+        self.open_project_page()
         wd.find_element('xpath', "//input[@value='Create New Project']").click()
         self.fill_project_form(project)
         self.submit_project()
@@ -43,59 +44,19 @@ class Project_helper:
             list.append(Project(name =project_name, id=project_id))
         return list
 
-    def delete_project_by_id(self):
-        pass
-    """def count(self):
-        wd = self.app.wd
-        self.open_project_page()
-        return len(wd.find_elements(By.XPATH, '//table[@class="width100"][2]//tbody//tr//class="row-"[1:]'))
-        #return len(wd.find_elements('name', "selected[]"))
-
-
-
     def delete_project_by_id(self, project):
         wd = self.app.wd
-        self.open_project_page_by_link()
-        wd.find_element_by_css_selector("a[href='manage_proj_edit_page.php?project_id=%s" % project.project_id).click()
-        wd.find_element_by_css_selector("input[value='Delete Project']").click()
-        time.sleep(0.5)
-        wd.find_element_by_css_selector("input[value='Delete Project']").click()"""
+        self.open_project_page()
+        wd.find_element(By.CSS_SELECTOR, "a[href='manage_proj_edit_page.php?project_id=%s" % project.id).click()
+        wd.find_element(By.CSS_SELECTOR, "input[value='Delete Project']").click()
+        time.sleep(1)
+        wd.find_element(By.CSS_SELECTOR, "input[value='Delete Project']").click()
 
-    """def get_group_list(self):
-        if self.group_cache is None:
-            wd = self.app.wd
-            self.open_groups_page()
-            self.group_cache = []
-            for element in wd.find_elements(By.CSS_SELECTOR, "span.group"):
-                # text = element.find_element('name', "selected[]").get_attribute("title")
-                text = element.text
-                id = element.find_element('name', "selected[]").get_attribute("value")
-                self.group_cache.append(Group(name=text, id=id))
 
-        # print(self.group_cache)
-        return list(self.group_cache)"""
 
-    """def get_projects_list(self):
-        wd = self.app.wd
-        self.open_manage_projects()
-        list = []
-        count = len(wd.find_elements_by_xpath('//table[@class="width100"][2]//tbody//tr'))
-        try:
-            for n in range(3, count + 1):
-                id = wd.find_element_by_xpath(f'//table[@class="width100"][2]//tbody//tr[{n}]//td//a').get_attribute(
-                    'href')
-                id = str(id).split("=")[1]
-                name = wd.find_element_by_xpath(f'//table[@class="width100"][2]//tbody//tr[{n}]//td//a').text
-                status = wd.find_element_by_xpath(f'//table[@class="width100"][2]//tbody//tr[{n}]//td[2]').text
-                enabled = wd.find_element_by_xpath(f'//table[@class="width100"][2]//tbody//tr[{n}]//td[3]').text
-                view_status = wd.find_element_by_xpath(f'//table[@class="width100"][2]//tbody//tr[{n}]//td[4]').text
-                description = wd.find_element_by_xpath(f'//table[@class="width100"][2]//tbody//tr[{n}]//td[5]').text
-                list.append(Project(id=id, name=name, status=status, enabled=enabled, view_status=view_status,
-                                    description=description))
-            return list
-        except:
-            return list
-"""
+
+
+
 
 
 
--- tests/test_add_new_project.py
+++ tests/test_add_new_project.py
@@ -3,16 +3,12 @@ from model.project import Project
 
 def test_add_project(app):
     app.session.login("administrator", "root")
-    app.project.open_project_page()
     old_projects = app.project.get_project_list()
-    #print(old_projects)
     project_name = ("Project_test " + str(random.randint(0, 100)))
     project_description = ("Description_test " + str(random.randint(0, 100)))
     app.project.create_project(Project(name=project_name, description = project_description))
     old_projects.append(Project(name=project_name, description = project_description))
-    #print("old projects appended", old_projects)
     new_projects = app.project.get_project_list()
-    #print("new projects", new_projects)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
 
 
