f8cc7f6e07a66b35c717c0148431524ff5eedfb0
Valeria Chikina <dliavajnogo@gmail.com>
2024-04-15 18:47:03 +0300

    added ftp stuff

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 import json
 import os.path
 from fixture.application import Application
-
+import ftputil
 # глобальная переменная для хранения фикстуры
 fixture = None
 target = None
@@ -19,16 +19,42 @@ def load_config(file):
 
 # инициализатор фикстуры подключения к вэбдрайверу
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))["web"]
     web_config_login = load_config(request.config.getoption("--target"))["webadmin"]
     if fixture is None or not fixture.is_valid():
-        fixture =Application(browser=browser, base_url=web_config["baseUrl"])
+        fixture =Application(browser=browser, base_url=config["web"]["baseUrl"])
     fixture.session.ensure_login(username=web_config_login["username"], password=web_config_login["password"])
     return fixture
 
+
+@pytest.fixture(scope = "session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+@pytest.fixture(scope = "session", autouse = True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc1.php"):
+            remote.remove("config_inc1.php")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc1.php")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc1.php"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc1.php", "config_inc.php")
 
 # фикстура выполнится несмотря на то, что нигде не указана
 # благодаря параметру autouse
