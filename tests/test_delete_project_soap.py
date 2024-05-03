267801201aa41faafa14a9bf1fbd92e8a701d05f
Valeria Chikina <dliavajnogo@gmail.com>
2024-05-03 11:22:39 +0300

    added test_delete_project_soap.py

---
+++ tests/test_delete_project_soap.py
@@ -0,0 +1,13 @@
+from model.project import Project
+import random
+
+def test_delete_project(app):
+    old_projects = app.soap.get_project_list()
+    if len(old_projects) == 0:
+        app.project.create_project(Project(name="test_name", description = "test_description"))
+    old_projects = app.soap.get_project_list()
+    project = random.choice(old_projects)
+    app.project.delete_project_by_id(project)
+    new_projects = app.soap.get_project_list()
+    old_projects.remove(project)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
